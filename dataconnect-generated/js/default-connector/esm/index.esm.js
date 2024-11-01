60d0fcf561965f6f3b041e9db0c1f5e63085c122
ycristian01 <ycristian@uninorte.edu.co>
2024-11-01 17:57:06 -0500

    deploy: firebase integration changes

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'resume-cristian-yepes',
+  location: 'us-central1'
+};
+
